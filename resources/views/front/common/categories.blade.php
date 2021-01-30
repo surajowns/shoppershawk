4f1d36dfa30223d7c7214e91fa7bd04862e9385b
PramodBesthawk <pramod@besthawk.com>
2021-01-30 14:49:44 +0530

    categories

--- resources/views/front/common/categories.blade.php
+++ resources/views/front/common/categories.blade.php
@@ -1,96 +1,21 @@
+<?php 
+  $categories=App\CategoryModel::where('parent_id',0)->where('status',1)->get();
+
+?>
 <div class="categories_product_area mb-55">
     <div class="container">
         <div class="categories_product_inner">
+        @foreach($categories as $cat)
             <div class="single_categories_product">
                 <div class="categories_product_content">
-                    <h4><a href="shop.html"> Cells & Tablets</a></h4>
+                    <h4><a href="shop.html">{{$cat['name']}}</a></h4>
                     <p>12 Products</p>
                 </div>
                 <div class="categories_product_thumb">
-                    <a href="shop.html"><img src="front/img/s-product/category1.jpg" alt="" /></a>
+                    <a href="shop.html"><img src="{{url('public/category/'.$cat['image'])}}" alt="" /></a>
                 </div>
             </div>
-            <div class="single_categories_product">
-                <div class="categories_product_content">
-                    <h4><a href="shop.html"> Computer</a></h4>
-                    <p>24 Products</p>
-                </div>
-                <div class="categories_product_thumb">
-                    <a href="shop.html"><img src="front/img/s-product/category2.jpg" alt="" /></a>
-                </div>
-            </div>
-            <div class="single_categories_product">
-                <div class="categories_product_content">
-                    <h4><a href="shop.html"> Fashion</a></h4>
-                    <p>22 Products</p>
-                </div>
-                <div class="categories_product_thumb">
-                    <a href="shop.html"><img src="front/img/s-product/category3.jpg" alt="" /></a>
-                </div>
-            </div>
-            <div class="single_categories_product">
-                <div class="categories_product_content">
-                    <h4><a href="shop.html"> Sunglasses</a></h4>
-                    <p>06 Products</p>
-                </div>
-                <div class="categories_product_thumb">
-                    <a href="shop.html"><img src="front/img/s-product/category4.jpg" alt="" /></a>
-                </div>
-            </div>
-            <div class="single_categories_product">
-                <div class="categories_product_content">
-                    <h4><a href="shop.html"> Baby & Kids</a></h4>
-                    <p>20 Products</p>
-                </div>
-                <div class="categories_product_thumb">
-                    <a href="shop.html"><img src="front/img/s-product/category5.jpg" alt="" /></a>
-                </div>
-            </div>
-            <div class="single_categories_product">
-                <div class="categories_product_content">
-                    <h4><a href="shop.html"> Accessories</a></h4>
-                    <p>04 Products</p>
-                </div>
-                <div class="categories_product_thumb">
-                    <a href="shop.html"><img src="front/img/s-product/category6.jpg" alt="" /></a>
-                </div>
-            </div>
-            <div class="single_categories_product">
-                <div class="categories_product_content">
-                    <h4><a href="shop.html"> Cells & Tablets</a></h4>
-                    <p>12 Products</p>
-                </div>
-                <div class="categories_product_thumb">
-                    <a href="shop.html"><img src="front/img/s-product/category7.jpg" alt="" /></a>
-                </div>
-            </div>
-            <div class="single_categories_product">
-                <div class="categories_product_content">
-                    <h4><a href="shop.html"> Accessories</a></h4>
-                    <p>12 Products</p>
-                </div>
-                <div class="categories_product_thumb">
-                    <a href="shop.html"><img src="front/img/s-product/category8.jpg" alt="" /></a>
-                </div>
-            </div>
-            <div class="single_categories_product">
-                <div class="categories_product_content">
-                    <h4><a href="shop.html"> Cells & Tablets</a></h4>
-                    <p>12 Products</p>
-                </div>
-                <div class="categories_product_thumb">
-                    <a href="shop.html"><img src="front/img/s-product/category9.jpg" alt="" /></a>
-                </div>
-            </div>
-            <div class="single_categories_product">
-                <div class="categories_product_content">
-                    <h4><a href="shop.html"> Baby & Kids</a></h4>
-                    <p>12 Products</p>
-                </div>
-                <div class="categories_product_thumb">
-                    <a href="shop.html"><img src="front/img/s-product/category10.jpg" alt="" /></a>
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </div>
