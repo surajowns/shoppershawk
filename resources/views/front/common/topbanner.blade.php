9ac77dc80b3670d004ae00d50ebc948d26e67377
PramodBesthawk <pramod@besthawk.com>
2021-01-30 10:42:08 +0530

    banners

--- resources/views/front/common/topbanner.blade.php
+++ resources/views/front/common/topbanner.blade.php
@@ -1,53 +1,25 @@
+<?php  $topbanner=App\BannerModel::where('status',1)->where('type','Top Banner')->get() ?>
 <section class="slider_section slider_s_one mb-60 mt-20">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-6 offset-lg-3 col-md-12">
                         <div class="swiper-container gallery-top">
                             <div class="slider_area swiper-wrapper">
-                                <div class="single_slider swiper-slide d-flex align-items-center" data-bgimg="front/img/slider/slider1.jpg">
-                                    <div class="slider_content">
-                                        <h3>popular products</h3>
-                                        <h1>
-                                            summer <br />
-                                            collection 2020
-                                        </h1>
-                                        <p>discount <span> -30% off</span> this week</p>
-                                        <a class="button" href="shop.html">DISCOVER NOW</a>
-                                    </div>
+                            @foreach($topbanner as $banner)
+                                <div class="single_slider swiper-slide d-flex align-items-center" data-bgimg="{{url('public/banner/'.$banner['banner_image'])}}">
+                                    <!-- <div class="slider_content"> -->
+                                        <!-- <h3>popular products</h3> -->
+                                        <!-- <h1> -->
+                                            <!-- summer <br />
+                                            collection 2020 -->
+                                        <!-- </h1> -->
+                                        <!-- <p>discount <span> -30% off</span> this week</p>
+                                        <a class="button" href="shop.html">DISCOVER NOW</a> -->
+                                    <!-- </div> -->
                                 </div>
-                                <div class="single_slider swiper-slide d-flex align-items-center" data-bgimg="front/img/slider/slider2.jpg">
-                                    <div class="slider_content">
-                                        <h3>big sale products</h3>
-                                        <h1>
-                                            wooden minimalist <br />
-                                            chair 2019
-                                        </h1>
-                                        <p>discount <span> -50% off</span> this week</p>
-                                        <a class="button" href="shop.html">DISCOVER NOW</a>
-                                    </div>
-                                </div>
-                                <div class="single_slider swiper-slide d-flex align-items-center" data-bgimg="front/img/slider/slider3.jpg">
-                                    <div class="slider_content color_white">
-                                        <h3>new arrivals</h3>
-                                        <h1>
-                                            business <br />
-                                            off mobile apps
-                                        </h1>
-                                        <p>discount <span> -10% off</span> this week</p>
-                                        <a class="button" href="shop.html">DISCOVER NOW</a>
-                                    </div>
-                                </div>
-                                <div class="single_slider swiper-slide d-flex align-items-center" data-bgimg="front/img/slider/slider4.jpg">
-                                    <div class="slider_content color_white">
-                                        <h3>new arrivals</h3>
-                                        <h1>
-                                            cellphone <br />
-                                            new model 2019
-                                        </h1>
-                                        <p>discount <span> -60% off</span> this week</p>
-                                        <a class="button" href="shop.html">DISCOVER NOW</a>
-                                    </div>
-                                </div>
+                            @endforeach
+                              
+                               
                             </div>
                             <!-- Add Arrows -->
 
@@ -55,19 +27,11 @@
                         </div>
                         <div class="swiper-container gallery-thumbs">
                             <div class="swiper-wrapper">
+                               @foreach($topbanner as $title)
                                 <div class="swiper-slide">
-                                    Eodem modo vel mattis ante nec
+                                    {{$title->title}}
                                 </div>
-                                <div class="swiper-slide">
-                                    <a href="#"></a>
-                                    Wooden Minimalist Chair 2019
-                                </div>
-                                <div class="swiper-slide">
-                                    Eodem modo vel mattis ante facilisis nec
-                                </div>
-                                <div class="swiper-slide">
-                                    Donec eu libero ac dapibus
-                                </div>
+                              @endforeach
                             </div>
                         </div>
                     </div>
@@ -76,7 +40,7 @@
                         <div class="sidebar_banner_area">
                             <figure class="single_banner mb-20">
                                 <div class="banner_thumb">
-                                    <a href="shop.html"><img src="front/img/bg/banner1.jpg" alt="" /></a>
+                                    <a href="shop.html"><img src="http://localhost/shoppershawk/public/banner/60028a19cc053_20210116.jpg" alt="" /></a>
                                 </div>
                             </figure>
                             <figure class="single_banner mb-20">
