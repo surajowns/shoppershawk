ddb99a2bba1e758bbd1d90d64d4c890aa535750c
PramodBesthawk <pramod@besthawk.com>
2021-02-15 18:00:09 +0530

    checkout

--- resources/views/front/common/checkout.blade.php
+++ resources/views/front/common/checkout.blade.php
@@ -157,7 +157,21 @@
                               </div>
                            </div>
                         </div>
-                     
+                        <div class="payment_method">
+                           <div class="panel-default">
+                              <input id="payment_defult" name="check_method" type="radio" data-target="createp_account" />
+                              <label for="payment_defult" data-bs-toggle="collapse" data-bs-target="#collapsedefult" aria-controls="collapsedefult">PayPal <img src="assets/img/icon/papyel.png" alt=""></label>
+                              <div id="collapsedefult" class="collapse one" data-parent="#accordion">
+                                 <div class="card-body1">
+                                    <p>Pay via PayPal; you can pay with your credit card if you don’t have a PayPal account.</p>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="order_button">
+                              <button type="submit">Proceed to PayPal</button>
+                           </div>
+                        </div>
+                     </form>
                   </div>
                </div>
                <div class="col-lg-6 col-md-6">
@@ -199,30 +213,7 @@
                               </tfoot>
                            </table>
                         </div>
-                        <div class="payment_method">
-                           <div class="panel-default">
-                              <input id="payment" name="check_method" type="radio" data-target="createp_account" />
-                              <label for="payment" data-bs-toggle="collapse" data-bs-target="#method" aria-controls="method">Create an account?</label>
-                              <div id="method" class="collapse one" data-parent="#accordion">
-                                 <div class="card-body1">
-                                    <p>Please send a check to Store Name, Store Street, Store Town, Store State / County, Store Postcode.</p>
-                                 </div>
-                              </div>
-                           </div>
-                           <div class="panel-default">
-                              <input id="payment_defult" name="check_method" type="radio" data-target="createp_account" />
-                              <label for="payment_defult" data-bs-toggle="collapse" data-bs-target="#collapsedefult" aria-controls="collapsedefult">PayPal <img src="assets/img/icon/papyel.png" alt=""></label>
-                              <div id="collapsedefult" class="collapse one" data-parent="#accordion">
-                                 <div class="card-body1">
-                                    <p>Pay via PayPal; you can pay with your credit card if you don’t have a PayPal account.</p>
-                                 </div>
-                              </div>
-                           </div>
-                           <div class="order_button">
-                              <button type="submit">Proceed to PayPal</button>
-                           </div>
-                        </div>
-                     </form>
+                     
                   </div>
                </div>
             </div>
