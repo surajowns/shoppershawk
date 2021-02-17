0beead0f7f176ad141b530e57fb8543be1c860bc
PramodBesthawk <pramod@besthawk.com>
2021-02-17 12:08:39 +0530

    slidder

--- resources/views/front/common/topslider.blade.php
+++ resources/views/front/common/topslider.blade.php
@@ -7,7 +7,7 @@ $singlebanner=App\BannerModel::where('status',1)->where('type',2)->get(); ?>
         <div class="owl-stage-outer">
             <div class="owl-stage" style="transform: translate3d(-2698px, 0px, 0px); transition: all 0s ease 0s; width: 9443px;">
                @foreach($topbanner as $slider)
-               <div class="owl-item active" style="width: 1349px;">
+               <div class="owl-item" style="width: 1349px;">
                     <div class="single_slider d-flex align-items-center" data-bgimg="{{url('public/banner/'.$slider['banner_image'])}}">
                         <div class="container">
                             <div class="row">
