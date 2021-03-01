9751965c07d33fef1b201134deca8a03e5c3e29b
PramodBesthawk <pramod@besthawk.com>
2021-03-01 13:58:28 +0530

    pages

--- resources/views/front/pages/informationpage.blade.php
+++ resources/views/front/pages/informationpage.blade.php
@@ -3,7 +3,13 @@
 @section('content')
 <div class="cart_page_bg">
         <div class="container">
-           {!! $page['description'] !!}
+          <div class="row">
+            <div class="col-sm-2"></div>
+            <div class="col-sm-8">
+               {!! $page['description'] !!}
+            </div>
+            <div class="col-sm-2"></div>
+          </div>
         </div>
 </div>
 @endsection
