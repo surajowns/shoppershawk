58dc352f2e419b545ae76d18548ab80d66af0bc3
PramodBesthawk <pramod@besthawk.com>
2021-04-10 12:58:17 +0530

    resetpassword admin

--- resources/views/admin/index/resetpassword.blade.php
+++ resources/views/admin/index/resetpassword.blade.php
@@ -3,23 +3,23 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-	<title>Shoppershawk | Login</title>
+	<title>Shoppershawk | ResetPassword</title>
 
 	<!-- Favicons -->
-	<link rel="shortcut icon" href="{{url('admin/assets/img/favicon.png')}}">
+	<link rel="shortcut icon" href="{{url('public/admin/assets/img/favicon.png')}}">
 
 	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="{{url('admin/assets/plugins/bootstrap/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{url('public/admin/assets/plugins/bootstrap/css/bootstrap.min.css')}}">
 
 	<!-- Fontawesome CSS -->
-	<link rel="stylesheet" href="{{url('admin/assets/plugins/fontawesome/css/fontawesome.min.css')}}">
-	<link rel="stylesheet" href="{{url('admin/assets/plugins/fontawesome/css/all.min.css')}}">
+	<link rel="stylesheet" href="{{url('public/admin/assets/plugins/fontawesome/css/fontawesome.min.css')}}">
+	<link rel="stylesheet" href="{{url('public/admin/assets/plugins/fontawesome/css/all.min.css')}}">
 	 
 	<!-- Animate CSS -->
-	<link rel="stylesheet" href="{{url('admin/assets/css/animate.min.css')}}">
+	<link rel="stylesheet" href="{{url('public/admin/assets/css/animate.min.css')}}">
 
 	<!-- Main CSS -->
-	<link rel="stylesheet" href="{{url('admin/assets/css/admin.css')}}">
+	<link rel="stylesheet" href="{{url('public/admin/assets/css/admin.css')}}">
 
 </head>
 
@@ -33,12 +33,12 @@
 						<div class="account-header">
 							<div class="account-logo text-center mb-4">
 								<a href="index.html">
-									<img src="{{url('admin/assets/img/logo-icon.png')}}" alt="" class="img-fluid">
+									<img src="{{url('public/admin/assets/img/logo-icon.png')}}" alt="" class="img-fluid">
 								</a>
 							</div>
 						</div>
 						<div class="login-header">
-							<h3>Forgot Password? <span>Best Hawk</span></h3>
+							<h3>Reset Password? <span></span></h3>
 						</div>
                         @if(session('failed'))
                                     <p class="text-center text-danger">{{session('failed')}}</p>
@@ -49,7 +49,7 @@
                         @if(session('success'))
                                     <p class="text-center text-success">{{session('success')}}</p>
                         @endif
-						<form action="{{url('admin/reset_password/'.$user['token'])}}" method="post" enctype="multipart/form-data">
+						<form id="resetpasswordform" action="{{url('admin/reset_password/'.$user['token'])}}" method="post" enctype="multipart/form-data">
                         {{csrf_field()}}
  
 							<div class="form-group mb-4">
@@ -59,12 +59,12 @@
 							<span class="text-danger">{{$errors->first('email')}}</span>
                             <div class="form-group mb-4">
 								<label class="control-label">Password</label>
-								<input class="form-control" type="text"  name="password" value="" required placeholder="Enter Password">
+								<input class="form-control" type="text"  name="password" id="password" value="" required placeholder="Enter Password">
 							</div>
 							<span class="text-danger">{{$errors->first('password')}}</span>
                             <div class="form-group mb-4">
 								<label class="control-label">Confirm Password</label>
-								<input class="form-control" type="text"  name="c_password" value="" required placeholder="Enter Confirm Password">
+								<input class="form-control" type="text"  name="c_password" id="confirmpassword" value="" required placeholder="Enter Confirm Password">
 							</div>
 							<span class="text-danger">{{$errors->first('c_password')}}</span>
 
@@ -72,7 +72,7 @@
 								<button class="btn btn-primary btn-block account-btn" type="submit">Submit</button>
 							</div>
 						</form>							
-								<div class="text-center dont-have">Remember your password? <a href="{{url('admin/login')}}">Login</a></div>
+								<div class="text-center dont-have">Remember your password? <a href="{{url('/login')}}">Login</a></div>
 					</div>
 				</div>
 			</div>
@@ -80,17 +80,61 @@
 	</div>
 
 	<!-- jQuery -->
-	<script src="{{url('admin/assets/js/jquery-3.5.0.min.js')}}"></script>
+	<script src="{{url('public/admin/assets/js/jquery-3.5.0.min.js')}}"></script>
 
 	<!-- Bootstrap Core JS -->
-	<script src="{{url('admin/assets/js/popper.min.js')}}"></script>
-	<script src="{{url('admin/assets/plugins/bootstrap/js/bootstrap.min.js')}}"></script>
+	<script src="{{url('public/admin/assets/js/popper.min.js')}}"></script>
+	<script src="{{url('public/admin/assets/plugins/bootstrap/js/bootstrap.min.js')}}"></script>
 
 	<!-- Slimscroll JS -->
-	<script src="{{url('admin/assets/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
+	<script src="{{url('public/admin/assets/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
 
 	<!-- Custom JS -->
-	<script src="{{url('admin/assets/js/admin.js')}}"></script>
+	<script src="{{url('public/admin/assets/js/admin.js')}}"></script>
+	<script src="{{url('public/front/js/jquery.validate.min.js')}}"></script>
+
+<script>
+$(document).ready(function(){
+
+
+	 if ($("#resetpasswordform").length > 0) {
+$("#resetpasswordform").validate({
+
+	rules: {
+		
+		email: {
+			required: true,
+			maxlength: 50,
+			email: true,
+		},
+		
+		password: {
+			required: true,
+			minlength:6,
+			
+		},
+		c_password: {
+			required: true,
+			equalTo : "#password",
+			
+		},
+	},
+	messages: {
+		
+		email: {
+			required: "Please enter valid email",
+			email: "Please enter valid email",
+			maxlength: "The email name should less than or equal to 50 characters",
+		},
+		c_password:{
+			equalTo : "Incorrect password",
+		}
+
+	},
+})
+}
+});
+</script>
 
 </body>
 </html>
