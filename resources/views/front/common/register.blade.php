cb6ebc30d9895aee259f5c2d43bc37270ef71d7d
PramodBesthawk <pramod@besthawk.com>
2021-04-17 12:27:28 +0530

    phone

--- resources/views/front/common/register.blade.php
+++ resources/views/front/common/register.blade.php
@@ -47,7 +47,7 @@
                             <div class="header_top_settings text-right">
                                 <ul>
                                     <li><a href="#">Track Your Order</a></li>
-                                    <li>Hotline: <a href="tel:+0120-2512786">+0120-2512786</a></li>
+                                    <li>Hotline: <a href="tel:+91 120-2514786">+91 120-2514786</a></li>
                                 </ul>
                                 <div class="order_button mt-20">
                                 @if(!Auth::check())
