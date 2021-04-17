1376a5959ca7e2463662b16a44df0634eb313d68
PramodBesthawk <pramod@besthawk.com>
2021-04-17 12:56:24 +0530

    phone

--- resources/views/front/common/cart.blade.php
+++ resources/views/front/common/cart.blade.php
@@ -128,7 +128,7 @@
                                         </div>
                                         @endif
                                         <div class="checkout_btn">
-                                         <a href="tel:+0120-2512786" class="btn btn-lg btn-block">Call Us 0120-2512786</a>
+                                         <a href="tel:+91 120-2514786" class="btn btn-lg btn-block">Call Us +91 120-2514786</a>
                                             <!-- <a href="{{url('/user/checkout')}}">Proceed to Checkout</a> -->
                                         </div>
                                     </div>
