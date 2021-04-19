2ee09a46b8fcf4800a3b7b492fcde106bf4a417e
PramodBesthawk <pramod@besthawk.com>
2021-04-19 10:46:56 +0530

    style

--- resources/views/front/pages/informationpage.blade.php
+++ resources/views/front/pages/informationpage.blade.php
@@ -4,11 +4,11 @@
 <div class="cart_page_bg">
         <div class="container">
           <div class="row">
-            <div class="col-sm-2"></div>
-            <div class="col-sm-8">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-10">
                {!! $page['description'] !!}
             </div>
-            <div class="col-sm-2"></div>
+            <div class="col-sm-1"></div>
           </div>
         </div>
 </div>
