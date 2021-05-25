96f567c520a21f06fcb3c2b6f7c8fe8d905407b1
PramodBesthawk <pramod@besthawk.com>
2021-05-25 17:17:17 +0530

    pwa

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -13,49 +13,49 @@ return [
         'status_bar'=> 'black',
         'icons' => [
             '72x72' => [
-                'path' => '/images/icons/icon-72x72.png',
+                'path' => 'public/images/icons/icon-72x72.png',
                 'purpose' => 'any'
             ],
             '96x96' => [
-                'path' => '/images/icons/icon-96x96.png',
+                'path' => 'public/images/icons/icon-96x96.png',
                 'purpose' => 'any'
             ],
             '128x128' => [
-                'path' => '/images/icons/icon-128x128.png',
+                'path' => 'public/images/icons/icon-128x128.png',
                 'purpose' => 'any'
             ],
             '144x144' => [
-                'path' => '/images/icons/icon-144x144.png',
+                'path' => 'public/images/icons/icon-144x144.png',
                 'purpose' => 'any'
             ],
             '152x152' => [
-                'path' => '/images/icons/icon-152x152.png',
+                'path' => 'public/images/icons/icon-152x152.png',
                 'purpose' => 'any'
             ],
             '192x192' => [
-                'path' => '/images/icons/icon-192x192.png',
+                'path' => 'public/images/icons/icon-192x192.png',
                 'purpose' => 'any'
             ],
             '384x384' => [
-                'path' => '/images/icons/icon-384x384.png',
+                'path' => 'public/images/icons/icon-384x384.png',
                 'purpose' => 'any'
             ],
             '512x512' => [
-                'path' => '/images/icons/icon-512x512.png',
+                'path' => 'public/images/icons/icon-512x512.png',
                 'purpose' => 'any'
             ],
         ],
         'splash' => [
-            '640x1136' => '/images/icons/splash-640x1136.png',
-            '750x1334' => '/images/icons/splash-750x1334.png',
-            '828x1792' => '/images/icons/splash-828x1792.png',
-            '1125x2436' => '/images/icons/splash-1125x2436.png',
-            '1242x2208' => '/images/icons/splash-1242x2208.png',
-            '1242x2688' => '/images/icons/splash-1242x2688.png',
-            '1536x2048' => '/images/icons/splash-1536x2048.png',
-            '1668x2224' => '/images/icons/splash-1668x2224.png',
-            '1668x2388' => '/images/icons/splash-1668x2388.png',
-            '2048x2732' => '/images/icons/splash-2048x2732.png',
+            '640x1136' => 'public/images/icons/splash-640x1136.png',
+            '750x1334' => 'public/images/icons/splash-750x1334.png',
+            '828x1792' => 'public/images/icons/splash-828x1792.png',
+            '1125x2436' => 'public/images/icons/splash-1125x2436.png',
+            '1242x2208' => 'public/images/icons/splash-1242x2208.png',
+            '1242x2688' => 'public/images/icons/splash-1242x2688.png',
+            '1536x2048' => 'public/images/icons/splash-1536x2048.png',
+            '1668x2224' => 'public/images/icons/splash-1668x2224.png',
+            '1668x2388' => 'public/images/icons/splash-1668x2388.png',
+            '2048x2732' => 'public/images/icons/splash-2048x2732.png',
         ],
         'shortcuts' => [
             [
@@ -70,7 +70,7 @@ return [
             [
                 'name' => 'Shortcut Link 2',
                 'description' => 'Shortcut Link 2 Description',
-                'url' => '/shortcutlink2'
+                'url' => '/'
             ]
         ],
         'custom' => []
