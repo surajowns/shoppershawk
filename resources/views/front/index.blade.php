5df06948fda7079ef8d964ea4df7448040a7bd87
PramodBesthawk <pramod@besthawk.com>
2021-05-31 12:10:04 +0530

    finale pwa

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -202,7 +202,7 @@
             <!-- categories  product area start-->
             <!-- categories product area end-->
             
-            <button class="add-button">Add Shoppers Hawk to Home screen</button>
+            <!-- <button class="add-button">Add Shoppers Hawk to Home screen</button> -->
 
         </div>
         <!--home section bg area end-->
@@ -260,34 +260,34 @@
                 .then(() => { //console.log('Service Worker Registered'); 
                 });
             }
-                   let deferredPrompt;
-                    const addBtn = document.querySelector('.add-button');
-                    addBtn.style.display = 'none';
+                //    let deferredPrompt;
+                //     const addBtn = document.querySelector('.add-button');
+                //     addBtn.style.display = 'none';
                     
-                    window.addEventListener('beforeinstallprompt', (e) => {
-                        // Prevent Chrome 67 and earlier from automatically showing the prompt
-                        e.preventDefault();
-                        // Stash the event so it can be triggered later.
-                        deferredPrompt = e;
-                        // Update UI to notify the user they can add to home screen
-                        addBtn.style.display = 'block';
+                //     window.addEventListener('beforeinstallprompt', (e) => {
+                //         // Prevent Chrome 67 and earlier from automatically showing the prompt
+                //         e.preventDefault();
+                //         // Stash the event so it can be triggered later.
+                //         deferredPrompt = e;
+                //         // Update UI to notify the user they can add to home screen
+                //         addBtn.style.display = 'block';
                     
-                        addBtn.addEventListener('click', () => {
-                        // hide our user interface that shows our A2HS button
-                        addBtn.style.display = 'none';
-                        // Show the prompt
-                        deferredPrompt.prompt();
-                        // Wait for the user to respond to the prompt
-                        deferredPrompt.userChoice.then((choiceResult) => {
-                            if (choiceResult.outcome === 'accepted') {
-                            console.log('User accepted the A2HS prompt');
-                            } else {
-                            console.log('User dismissed the A2HS prompt');
-                            }
-                            deferredPrompt = null;
-                        });
-                        });
-                    });
+                //         addBtn.addEventListener('click', () => {
+                //         // hide our user interface that shows our A2HS button
+                //         addBtn.style.display = 'none';
+                //         // Show the prompt
+                //         deferredPrompt.prompt();
+                //         // Wait for the user to respond to the prompt
+                //         deferredPrompt.userChoice.then((choiceResult) => {
+                //             if (choiceResult.outcome === 'accepted') {
+                //             console.log('User accepted the A2HS prompt');
+                //             } else {
+                //             console.log('User dismissed the A2HS prompt');
+                //             }
+                //             deferredPrompt = null;
+                //         });
+                //         });
+                //     });
 
                 @if(Session::has('success'))
                     toastr.success("{{Session::get('success')}}")
