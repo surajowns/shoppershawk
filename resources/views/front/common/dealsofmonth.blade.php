3a727b080e3a17724f3a0b8995e4788c9697bf15
PramodBesthawk <pramod@besthawk.com>
2021-06-29 12:50:19 +0530

    image zoom

--- resources/views/front/common/dealsofmonth.blade.php
+++ resources/views/front/common/dealsofmonth.blade.php
@@ -42,9 +42,12 @@
                     <article class="single_product {{$productdetails['qty']<=0?'not_in_stock':''}}"  >
                         <figure>
                             <div class="product_thumb">
+                           
                             @if(!empty($productdetails['product_image']))
                                 <a class="primary_img" href="{{url('/product_details/'.$productdetails['slug'])}}"><img src="{{url('public/product_image/'.$productdetails['product_image'][0]['image'])}}" alt="" /></a>
-                                <!-- <a class="secondary_img" href="#"><img src="{{url('public/product_image/'.$productdetails['product_image'][0]['image'])}}" alt="" /></a> -->
+                                   @if(array_key_exists(1,$productdetails['product_image']))
+                                     <a class="secondary_img" href="{{url('/product_details/'.$productdetails['slug'])}}"><img src="{{url('public/product_image/'.$productdetails['product_image'][1]['image'])}}" alt="" /></a>
+                                   @endif
                              @endif
                                 <!-- <div class="label_product">
                                     <span class="label_sale">Sale</span>
