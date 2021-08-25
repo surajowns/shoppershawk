a8fb1ee3e209263a398b86e859681d524f38235b
PramodBesthawk <pramod@besthawk.com>
2021-08-25 16:12:21 +0530

    product qtys

--- resources/views/admin/product/view.blade.php
+++ resources/views/admin/product/view.blade.php
@@ -52,7 +52,7 @@
 
                                <td>{{$loop->iteration}}</td>
                                <td><img src="{{url('public/product_image/'.$product['product_image'][0]['image'])}}" alt="" width="100" height="100"></td>
-                               <td style="width: 345px !important; overflow: auto;display: flex;">{{$product['name']}}</td>
+                               <td>{{$product['name']}}</td>
                                <td>{{$product['category']['name']}}</td>
                                <td>{{$product['product_brand']['name']}}</td>
                                <td>{{$product['model_no']}}</td>
