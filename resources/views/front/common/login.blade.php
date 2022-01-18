a35548087b6a0f13153cb860729cb1a7fc6a6485
PramodBesthawk <pramod@besthawk.com>
2022-01-18 16:42:56 +0530

    login content

--- resources/views/front/common/login.blade.php
+++ resources/views/front/common/login.blade.php
@@ -4,8 +4,8 @@
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="x-ua-compatible" content="ie=edge" />
-        <title>Shoppershawk</title>
-        <meta name="description" content="" />
+        <title>Shoppers Hawk</title>
+        <meta name="description" content="Shoppershawk.com: Online Shopping India - Buy laptops,desktops,monitors,apparel,accessories,Free Shipping Available." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <!-- Favicon -->
         <link rel="shortcut icon" type="image/x-icon" href="{{url('public/front/img/favicon.png')}}" />
